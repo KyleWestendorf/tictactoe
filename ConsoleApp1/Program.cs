8aebc756590880f5493cd1221e26de3a40cf0acc
KyleWestendorf <kylewestendorf@Gmail.com>
2018-01-09 00:34:52 -0500

    working AI to some extent, however, have to fix AI's ability to overwrite a square that's already been chosen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -178,61 +178,183 @@ namespace ConsoleApp1
 
             }
 
-        private static void AiTurn(string[] board, int counter, string playerTwo)
+        private  static void AiTurn(string[] board, int counter, string playerTwo)
         {
-            if (board[4] == "4" && counter == 1)
+            string move = CheckWinningMove(board);
+            Console.WriteLine("This is the move: {0}", move);
+            int AIChoice = 0;
+
+            Int32.TryParse(move, out AIChoice);
+            
+            if (move != "")
+            {
+                board[AIChoice] = playerTwo;
+            }
+            else if (board[4] == "4" && counter == 1)
             {
                 board[4] = playerTwo;
             }
+            else if (board[0] == "0")
+            {
+                board[0] = playerTwo;
+            }
+            else if (board[8] == "0")
+            {
+                board[0] = playerTwo;
+            }
+        }
+
+        private static string CheckWinningMove(string[] board)
+        {
+            string winner = "";
+            if (board[0] == board[1])
+            {
+                winner = board[2];
+            }
+            else if (board[1] == board[2])
+            {
+                winner = board[0];
+            }
+            else if (board[0] == board[2])
+            {
+                winner = board[1];
+            }
+            else if (board[3] == board[4])
+            {
+                winner = board[5];
+            }
+            else if (board[4] == board[5])
+            {
+                winner = board[3];
+            }
+            else if (board[3] == board[5])
+            {
+                winner = board[4];
+            }
+            else if (board[6] == board[7])
+            {
+                winner = board[8];
+            }
+            else if (board[7] == board[8])
+            {
+                winner = board[6];
+            }
+            else if (board[8] == board[6])
+            {
+                winner = board[7];
+            }
+            if (board[0] == board[3])
+            {
+                winner = board[6];
+            }
+            else if (board[3] == board[6])
+            {
+                winner = board[0];
+            }
+            else if (board[0] == board[6])
+            {
+                winner = board[3];
+            }
+            else if (board[1] == board[4])
+            {
+                winner = board[7];
+            }
+            else if (board[4] == board[7])
+            {
+                winner = board[1];
+            }
+            else if (board[1] == board[7])
+            {
+                winner = board[4];
+            }
+            else if (board[2] == board[5])
+            {
+                winner = board[8];
+            }
+            else if (board[5] == board[8])
+            {
+                winner = board[2];
+            }
+            else if (board[8] == board[2])
+            {
+                winner = board[5];
+            }
+            else if (board[0] == board[4])
+            {
+                winner = board[8];
+            }
+            else if (board[0] == board[8])
+            {
+                winner = board[4];
+            }
+            else if (board[4] == board[8])
+            {
+                winner = board[0];
+            }
+            else if (board[2] == board[4])
+            {
+                winner = board[6];
+            }
+            else if (board[2] == board[6])
+            {
+                winner = board[4];
+            }
+            else if (board[4] == board[6])
+            {
+                winner = board[2];
+            }
+            return winner;
         }
 
         private static void CheckWin(string[] board, int counter)
             {
-                if (board[0] == board[1] && board[1] == board[2])
-                {
-                    Winner();
-                }
-                else if (board[3] == board[4] && board[4] == board[5])
-                {
-                    Winner();
+            if (board[0] == board[1] && board[1] == board[2])
+            {
+                Winner(board);
             }
-                else if (board[6] == board[7] && board[7] == board[8])
-                {
-                    Winner();
+            else if (board[3] == board[4] && board[4] == board[5])
+            {
+                Winner(board);
             }
-                else if (board[0] == board[3] && board[3] == board[6])
-                {
-                    Winner();
+            else if (board[6] == board[7] && board[7] == board[8])
+            {
+                Winner(board);
             }
-                else if (board[1] == board[4] && board[4] == board[7])
-                {
-                     Winner();
+            else if (board[0] == board[3] && board[3] == board[6])
+            {
+                Winner(board);
             }
-                else if (board[2] == board[5] && board[5] == board[8])
-                {
-                     Winner();
+            else if (board[1] == board[4] && board[4] == board[7])
+            {
+                Winner(board);
             }
-                else if (board[0] == board[4] && board[4] == board[8])
-                {
-                     Winner();
+            else if (board[2] == board[5] && board[5] == board[8])
+            {
+                Winner(board);
             }
-                else if (board[2] == board[4] && board[4] == board[6])
-                {
-                     Winner();
-                
-            } else if (counter >= 8)
-                {
-                    Console.WriteLine("Draw!");
-                    Console.ReadLine();
-                    Replay();
-                }
+            else if (board[0] == board[4] && board[4] == board[8])
+            {
+                Winner(board);
+            }
+            else if (board[2] == board[4] && board[4] == board[6])
+            {
+                Winner(board);
+
+            }
+            else if (counter >= 8)
+            {
+                Console.WriteLine("Draw!");
+                Console.ReadLine();
+                Replay();
+            }
 
 
             }
 
-        private static void Winner()
+        private static void Winner(string[] board)
         {
             Console.WriteLine("Winner!");
+            ShowBoard(board);
             Console.ReadLine();
             Replay();
         }
